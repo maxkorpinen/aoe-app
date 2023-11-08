781e1a989c5c102958ebcfc32c669b824f2d46ae
Sampo Hakola <sampo.hakola@helsinki.fi>
2023-11-08 19:20:02 +0200

    test backend määritetty-> toimisko testit actionssissa

--- aoe-front/src/utils/apiClient.js
+++ aoe-front/src/utils/apiClient.js
@@ -7,9 +7,19 @@ if (process.env.NODE_ENV === 'development') {
   BACKEND = process.env.REACT_APP_BACKEND_URL_DEV
 } */
 
-const BACKEND = process.env.NODE_ENV === 'development'
+/* const BACKEND = process.env.NODE_ENV === 'development'
   ? process.env.REACT_APP_BACKEND_URL_DEV
-  : ''
+  : '' */
+let BACKEND = ''
+switch(process.env.NODE_ENV) {
+  case 'development':
+    BACKEND = process.env.REACT_APP_BACKEND_URL_DEV
+  case 'test':
+    //BACKEND = process.env.BACKEND_URL_TEST
+    BACKEND = 'http://localhost:3001'
+  default:
+    BACKEND = process.env.REACT_APP_BACKEND_URL_DEV
+}
 
 const apiClient = axios.create({
   baseURL: BACKEND
