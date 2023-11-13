1e1e32cf49337934496e65a229d3dda4c1961f07
Sampo Hakola <sampo.hakola@helsinki.fi>
2023-11-14 01:59:18 +0200

    eslint korjaus #skip #deploy

--- aoe-front/src/utils/apiClient.js
+++ aoe-front/src/utils/apiClient.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-let BACKEND = process.env.REACT_APP_BACKEND_URL_DEV 
+let BACKEND = process.env.REACT_APP_BACKEND_URL_DEV
 
 const apiClient = axios.create({
   baseURL: BACKEND
