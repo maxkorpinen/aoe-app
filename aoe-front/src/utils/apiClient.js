1b49679a97f288e651269bd54438fa02e55fafd9
Sampo Hakola <sampo.hakola@helsinki.fi>
2023-11-14 15:42:31 +0200

    flybuild, apiclient samaan kuntoon kuin missä deploy toimi

--- aoe-front/src/utils/apiClient.js
+++ aoe-front/src/utils/apiClient.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-let BACKEND = process.env.REACT_APP_BACKEND_URL_DEV
+const BACKEND = process.env.NODE_ENV === 'development'
+  ? process.env.REACT_APP_BACKEND_URL_DEV
+  : ''
 
 const apiClient = axios.create({
   baseURL: BACKEND
