f095c6b79662a05b8d799491a671c92489b0b14c
Sampo Hakola <sampo.hakola@helsinki.fi>
2024-02-10 12:13:59 +0200

    Deleted test that depended on on old structures, test should work now

--- aoe-back/controllers/civs.js
+++ aoe-back/controllers/civs.js
@@ -5,36 +5,36 @@ const {punit, getPowerUnit, getCounters} = require('../utils/CivUtils')
 // Endpoint for GETting all civs
 router.get('/', async (req, res) => {
   try {
-    const civs = await Civ.find({});
-    let ret = [];
+    const civs = await Civ.find({})
+    let ret = []
     for (const [key, value] of Object.entries(civs)) {
-      let cur = civs[key];
+      let cur = civs[key]
       let newCivObj = {
         id: cur.id,
         name: cur.name,
         description: cur.description,
         units: cur.units,
         image: cur.image,
-      };
+      }
       ret.push(newCivObj);
     }
     res.json(ret);
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ message: error.message })
   }
 });
 
 // Endpoint for GETting a civ by id
 router.get('/:id', async (req, res) => {
   try {
-    const id = req.params.id;
-    const civ = await Civ.find({ _id: id });
+    const id = req.params.id
+    const civ = await Civ.find({ _id: id })
     if (!civ) {
-      return res.status(404).json({ message: 'Civ not found' });
+      return res.status(404).json({ message: 'Civ not found' })
     }
-    res.json(civ);
+    res.json(civ)
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ message: error.message })
   }
 });
 
