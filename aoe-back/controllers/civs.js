a356b6eccb514302b03c84d3ec6e7a73d3f15685
Sampo Hakola <sampo.hakola@helsinki.fi>
2024-03-19 12:22:06 +0200

    testing winstat endpoint

--- aoe-back/controllers/civs.js
+++ aoe-back/controllers/civs.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const Civ = require('../schemas/civ')
 const {punit, getPowerUnit, getCounters} = require('../utils/CivUtils')
+const Winstats = require('../schemas/winstats')
 
 // Endpoint for GETting all civs
 router.get('/', async (req, res) => {
@@ -34,6 +35,17 @@ router.get('/:id/powerunit', async (req, res) => {
   //const counters = getCounters(powerunit.unit)
   //powerunit.counters = counters
   res.send(powerunit)
+})
+
+router.get('/test', async (req, res) => { //'/:id1/vs/:id2'
+  const id1 = req.params.id1
+  const id2 = req.params.id2
+  //65a3d54e904c90227b6641f9
+  //65a3d695904c90227b66422a
+  const data = await Winstat.find({})
+  const civ1 = await Civ.find({ _id:id1})
+  const civ2 = await Civ.find({ _id:id2})
+  const dada = JSON.parse(data[0].data)
 })
 
 module.exports = router
